6ebd4da5889fc0376862d78f94aded1b3d2764c4
Ae Lim Lee <dofla0128@gmail.com>
2025-06-04 14:59:24 +0900

    Update vite.config.ts
    CD added

--- bank-frontend/vite.config.ts
+++ bank-frontend/vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // S3 업로드용
+    emptyOutDir: true, // 기존 파일 제거 후 빌드
+  },
+  base: '/', // 루트 경로에서 index.html 접근 (S3 정적 호스팅용)
 })
